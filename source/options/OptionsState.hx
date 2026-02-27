c1a5e2c4e73d88f97df4481cbdcf43ebc96f4f24
affsveyojuro-ctrl <affsveyojuro@gmail.com>
2026-02-26 23:14:34 -0300

    Update OptionsState.hx

--- source/options/OptionsState.hx
+++ source/options/OptionsState.hx
@@ -12,7 +12,6 @@ class OptionsState extends MusicBeatState
 		'Graphics',
 		'Visuals',
 		'Gameplay'
-		#if TRANSLATIONS_ALLOWED , 'Language' #end
 	];
 	private var grpOptions:FlxTypedGroup<Alphabet>;
 	private static var curSelected:Int = 0;
